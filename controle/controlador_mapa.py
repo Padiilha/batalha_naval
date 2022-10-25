1e67243a443b6fa625891aaf8b61509ddf23003f
Pedro Rocha <pedro.rocha2609@gmail.com>
2022-10-24 22:05:37 -0300

    Implementando Composição Navio em ControladorMapa, etindade Navio e tela_mapa

--- controle/controlador_mapa.py
+++ controle/controlador_mapa.py
@@ -1,5 +1,9 @@
+import random
+
 from modelo.mapa import Mapa
+from modelo.navio import TipoNavio, Navio
 from visualizacao.tela_mapa import TelaMapa
+
 
 class ControladorMapa:
     def __init__(self, controlador_principal):
@@ -7,12 +11,19 @@ class ControladorMapa:
         self.__mapas = []
         self.__tela_mapa = TelaMapa()
 
-
     def inclui_mapa(self):
-        dados = self.__tela_mapa.pega_dados_mapa()
+        dados = self.__tela_mapa.pega_dados_mapas()
         mapa = Mapa(dados["tamanho_x"],
                     dados["tamanho_y"])
-        if not self.ja_existe_mapa(mapa.tamanho_x, mapa.tamanho_y):
+
+        if not self.ja_existe_mapa(mapa['tamanho_x'], mapa["tamanho_y"]):
+            i = 0
+            tipos_navios = [TipoNavio.SUBMARINO, TipoNavio.FRAGATA, TipoNavio.NAVIOS_TANQUES, TipoNavio.PORTA_AVIOES]
+            while len(mapa.navios) < 4:
+                tipo_sorteado = random.choice(tipos_navios)
+                navio = Navio(id, tipo_sorteado)
+                mapa.navios.append(navio)
+                i += 1
             self.__mapas.append(mapa)
 
     def ja_existe_mapa(self, tamanho_x, tamanho_y):
@@ -28,13 +39,12 @@ class ControladorMapa:
             self.__tela_mapa.mostra_mapas(dados_mapa)
 
     def altera_mapas(self):
-        dados_mapa_antigo = self.__tela_mapa.pega_dados_altera_mapa()
-        novo_mapa = Mapa(dados_mapa_antigo["tamanho_x"],
-                         dados_mapa_antigo["tamanho_y"])
+        dados_mapa = self.__tela_mapa.pega_dados_altera_mapa()
+        novo_mapa = Mapa(dados_mapa["tamanho_x"],
+                         dados_mapa["tamanho_y"])
         for mapa in self.__mapas:
-            if mapa.id == dados_mapa_antigo["id"]:
-                mapa.tamanho_x = novo_mapa.tamanho_x
-                mapa.tamanho_y = novo_mapa.tamanho_y
+            if mapa.id == dados_mapa["id"]:
+                self.__mapas.remove()
 
     def remove_mapa(self):
         dados_mapa = self.__tela_mapa.pega_dados_e_deleta_mapa()
@@ -42,16 +52,25 @@ class ControladorMapa:
             if mapa.id == dados_mapa.id:
                 self.__mapas.remove(mapa)
 
+    def inclui_navio(self, mapa, tipo):
 
-    def inclui_navio(self, tipo, tamanho):
-        pass
+
+    def lista_navios(self):
+        id_mapa_escolhido = self.__tela_mapa.escolhe_mapa_contem_navios()
+        for mapa in self.__mapas:
+            if mapa.id == id_mapa_escolhido['id']:
+                for navio_no_mapa in mapa.navios:
+                    dados_navio = {"tamanho_navio": navio_no_mapa.tipo.value, "tipo": navio_no_mapa.tipo.name}
+                    self.__tela_mapa.mostra_navios(dados_navio)
 
     def mostra_tela_opcoes(self):
         opcoes = {
             1: self.inclui_mapa,
             2: self.lista_mapas,
             3: self.altera_mapas,
-            4: self.remove_mapa
+            4: self.remove_mapa,
+            5: self.inclui_navio,
+            6: self.lista_navios,
         }
         while True:
             opcao = self.__tela_mapa.mostrar_opcoes()
---
+++ modelo/navio.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+class TipoNavio(Enum):
+    SUBMARINO = 2
+    FRAGATA = 3
+    NAVIOS_TANQUES = 4
+    PORTA_AVIOES = 5
+
+class Navio:
+    def __init__(self, id, tipo: TipoNavio):
+        self.id = id
+        self.tipo = tipo
+
+#navio = Navio(10, TipoNavio.SUBMARINO)
+#navio.tipo.value
+#navio.tipo.name
